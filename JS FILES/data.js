ae0fe41b6a4974a087e5ef76da70de83d5142ee4
bryan-cucho1987 <bryancucho@gmail.com>
2023-10-07 17:50:26 +0200

    code review 2

---
+++ JS FILES/data.js
@@ -0,0 +1,57 @@
+let plan = `[
+    {
+        "task": "Shopping for the week",
+        "img": "IMAGES/shopping.jpeg",
+        "description": "Attention to seasonal veggies and dont forget to get extra shopping to cook Lasagna and tapas",
+        "importance": 0,
+        "dead_line":"12.10.2023"
+    },{
+        "task": "Coding",
+        "img": "IMAGES/Coding2.jpeg",
+        "description": "Watch the code overview before getting started with the exercises and challenges",
+        "importance": 0,
+        "dead_line": "06.10.2023"
+    },{
+        "task": "Cleaning the kitchen",
+        "img": "IMAGES/KITCHEN.jpeg",
+        "description": "Dust from top to bottom, clean small appliances, clean the sink and clean out the fridge",
+        "importance": 0,
+        "dead_line": "15.10.2023"
+    },{
+        "task": "Cleaning the Bathroom",
+        "img": "IMAGES/Bathroom.jpeg",
+        "description": "Dust and vacuum. Wipe mirrors and glass, disinfect countertops, surface areas, sinks and toilets",
+        "importance": 0,
+        "dead_line": "16.10.2023"
+    },{
+        "task": "Calling mum",
+        "img": "IMAGES/Call mum.jpeg",
+        "description": "Dont forget to send a Birthday card before calling",
+        "importance": 0,
+        "dead_line": "15.10.2023"
+    },{
+        "task": "Doing the laundry",
+        "img": "IMAGES/Laundryy.jpeg",
+        "description": "Check the care labels and sort the laundry by color, use all-purpose laundry detergent",
+        "importance": 0,
+        "dead_line": "15.10.2023"
+    },{
+        "task": "Go for a 10 km run",
+        "img": "IMAGES/RUN1.jpeg",
+        "description": "Remenber to set up the fitbit app to count the distance before starting the run",
+        "importance": 0,
+        "dead_line": "16.10.2023"
+    },{
+        "task": "Exercising",
+        "img": "IMAGES/exercise 2.jpeg",
+        "description": "Do a circuit workout that includes exercises like squats, lunges, push-ups, and sit-ups",
+        "importance": 0,
+        "dead_line": "08.10.2023"
+    },{
+        "task": "Submit code review on LMS",
+        "img": "IMAGES/Coding2.jpeg",
+        "description": "Do not forget to also submit on LMS and not only in github!",
+        "importance": 0,
+        "dead_line": "07.10.2023"
+    }
+]`;
---
+++ JS FILES/script.js
@@ -0,0 +1,63 @@
+let new_plan = JSON.parse(plan);
+console.log(new_plan);
+
+new_plan.forEach(element => {
+    document.getElementById("result").innerHTML += `
+<div class="card1 bg-white">
+<div class="card2 d-flex">
+  <div class="p-2 flex-grow-1"><button type="button" class="btn btn-primary position-relative"> Task </button></div>
+  <div class="p-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bookmark" viewBox="0 0 16 16">
+  <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/>
+</svg></div>
+  <div class="p-2"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+</svg></div>
+</div>
+
+  <img src="${element.img}" class="card-img-top " alt="${element.task}">
+  <div class="card-body">
+    <h5 class="card-title">${element.task}</h5>
+    <p class="card-text">${element.description}.</p>
+  </div>
+  <hr>
+  <ul class="list">
+    <li class="list-group-item"><b> Importance:</b><button class="btn btn-primary myBtn"><span>${element.importance}</span></button></li>
+    <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar2-week" viewBox="0 0 16 16">
+    <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z"/>
+    <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4zM11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"/>
+  </svg> <b>Dead line:</b> ${element.dead_line}</li>
+  </ul>
+  <hr>
+  <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+  <button class="btn btn-danger me-md-2" type="button"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+</svg> Delete</button>
+  <button class="btn btn-success" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+</svg> Done</button>
+</div>
+</div>`;
+});
+
+
+// i have tried so many times but it does not Work. i cant find the error in the console log. it says javascript but can find the error. I declared the class in so many ways but would not work. wondering if the problem is with JSON conversion or just a typo.
+//for this reason i got stucked and could not continue.. very frustrating... :(
+
+// let btns = document.querySelectorAll(".myBtn");
+// btns.forEach((btn, i) => {
+//     btn.addEventListener("click",)
+// });
+
+// function increaseImportance(indexOftheBtn) {
+//     new_plan[indexOftheBtn].importance++;
+//     console.log(new_plan[indexOftheBtn])
+
+
+
+
+
+
+
+
